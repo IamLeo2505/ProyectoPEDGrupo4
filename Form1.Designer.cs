327b69f816d5a9f62d099c3da70ba6a7a54179f7
LeandroMatus <matusleandro12@gmail.com>
2024-10-19 12:04:48 -0600

    PAGINA PRINCIPAL

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -28,12 +28,152 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            panel1 = new Panel();
+            panel2 = new Panel();
+            panel3 = new Panel();
+            btnColas = new Button();
+            button1 = new Button();
+            button2 = new Button();
+            button3 = new Button();
+            button4 = new Button();
+            button5 = new Button();
+            button6 = new Button();
+            button7 = new Button();
+            panel1.SuspendLayout();
+            panel2.SuspendLayout();
+            panel3.SuspendLayout();
+            SuspendLayout();
+            // 
+            // panel1
+            // 
+            panel1.Controls.Add(button1);
+            panel1.Controls.Add(btnColas);
+            panel1.Location = new Point(26, 32);
+            panel1.Name = "panel1";
+            panel1.Size = new Size(417, 766);
+            panel1.TabIndex = 0;
+            // 
+            // panel2
+            // 
+            panel2.Controls.Add(button5);
+            panel2.Controls.Add(button4);
+            panel2.Controls.Add(button3);
+            panel2.Controls.Add(button2);
+            panel2.Location = new Point(501, 32);
+            panel2.Name = "panel2";
+            panel2.Size = new Size(767, 766);
+            panel2.TabIndex = 1;
+            // 
+            // panel3
+            // 
+            panel3.Controls.Add(button7);
+            panel3.Controls.Add(button6);
+            panel3.Location = new Point(1333, 32);
+            panel3.Name = "panel3";
+            panel3.Size = new Size(417, 766);
+            panel3.TabIndex = 1;
+            // 
+            // btnColas
+            // 
+            btnColas.Location = new Point(148, 302);
+            btnColas.Name = "btnColas";
+            btnColas.Size = new Size(94, 29);
+            btnColas.TabIndex = 0;
+            btnColas.Text = "Iniciar";
+            btnColas.UseVisualStyleBackColor = true;
+            // 
+            // button1
+            // 
+            button1.Location = new Point(148, 717);
+            button1.Name = "button1";
+            button1.Size = new Size(94, 29);
+            button1.TabIndex = 1;
+            button1.Text = "Iniciar";
+            button1.UseVisualStyleBackColor = true;
+            // 
+            // button2
+            // 
+            button2.Location = new Point(141, 302);
+            button2.Name = "button2";
+            button2.Size = new Size(94, 29);
+            button2.TabIndex = 1;
+            button2.Text = "Iniciar";
+            button2.UseVisualStyleBackColor = true;
+            // 
+            // button3
+            // 
+            button3.Location = new Point(550, 302);
+            button3.Name = "button3";
+            button3.Size = new Size(94, 29);
+            button3.TabIndex = 2;
+            button3.Text = "Iniciar";
+            button3.UseVisualStyleBackColor = true;
+            // 
+            // button4
+            // 
+            button4.Location = new Point(141, 717);
+            button4.Name = "button4";
+            button4.Size = new Size(94, 29);
+            button4.TabIndex = 3;
+            button4.Text = "Iniciar";
+            button4.UseVisualStyleBackColor = true;
+            // 
+            // button5
+            // 
+            button5.Location = new Point(550, 717);
+            button5.Name = "button5";
+            button5.Size = new Size(94, 29);
+            button5.TabIndex = 4;
+            button5.Text = "Iniciar";
+            button5.UseVisualStyleBackColor = true;
+            // 
+            // button6
+            // 
+            button6.Location = new Point(176, 302);
+            button6.Name = "button6";
+            button6.Size = new Size(94, 29);
+            button6.TabIndex = 1;
+            button6.Text = "Iniciar";
+            button6.UseVisualStyleBackColor = true;
+            // 
+            // button7
+            // 
+            button7.Location = new Point(176, 717);
+            button7.Name = "button7";
+            button7.Size = new Size(94, 29);
+            button7.TabIndex = 2;
+            button7.Text = "Iniciar";
+            button7.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            BackColor = Color.MediumTurquoise;
+            ClientSize = new Size(1776, 823);
+            Controls.Add(panel3);
+            Controls.Add(panel2);
+            Controls.Add(panel1);
+            Name = "Form1";
+            Text = "Form1";
+            panel1.ResumeLayout(false);
+            panel2.ResumeLayout(false);
+            panel3.ResumeLayout(false);
+            ResumeLayout(false);
         }
 
         #endregion
+
+        private Panel panel1;
+        private Button button1;
+        private Button btnColas;
+        private Panel panel2;
+        private Button button5;
+        private Button button4;
+        private Button button3;
+        private Button button2;
+        private Panel panel3;
+        private Button button7;
+        private Button button6;
     }
 }
