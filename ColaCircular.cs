5daf36e6bec59d2a4bfe1712ab2ff7689f9d2176
Yoandri Núñez <yoandrynunezgarcia@gmail.com>
2024-10-26 04:28:19 -0600

    Creacion del Formulario Colas Circulares, Clase Cola Circular y la Clase ProductoColasCirculares

---
+++ ColaCircular.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProyectoFinalEstructuraDatosGrupo4
+{
+    internal class ColaCircular
+    {
+        private ProductoColasCirculares[] cola;
+        private int frente;
+        private int final;
+        private int tamaño;
+        private int contador;
+
+        public ColaCircular(int tamañoMaximo)
+        {
+            tamaño = tamañoMaximo;
+            cola = new ProductoColasCirculares[tamaño];
+            frente = -1;
+            final = -1;
+            contador = 0;
+        }
+
+        public bool EstaVacia() => contador == 0;
+        public bool EstaLlena() => contador == tamaño;
+
+        public void Añadir(ProductoColasCirculares item)
+        {
+            if (EstaLlena())
+            {
+                throw new InvalidOperationException("La cola está llena.");
+            }
+
+            if (EstaVacia())
+            {
+                frente = 0;
+            }
+
+            final = (final + 1) % tamaño;
+            cola[final] = item;
+            contador++;
+        }
+
+        public ProductoColasCirculares Eliminar()
+        {
+            if (EstaVacia())
+            {
+                throw new InvalidOperationException("La cola está vacía.");
+            }
+
+            ProductoColasCirculares item = cola[frente];
+            frente = (frente + 1) % tamaño;
+            contador--;
+
+            if (EstaVacia())
+            {
+                frente = -1;
+                final = -1;
+            }
+
+            return item;
+        }
+
+        public ProductoColasCirculares VerFrente()
+        {
+            if (EstaVacia())
+            {
+                throw new InvalidOperationException("La cola está vacía.");
+            }
+            return cola[frente];
+        }
+
+        public ProductoColasCirculares VerFinal()
+        {
+            if (EstaVacia())
+            {
+                throw new InvalidOperationException("La cola está vacía.");
+            }
+            return cola[final];
+        }
+
+        public List<ProductoColasCirculares> ObtenerElementos()
+        {
+            List<ProductoColasCirculares> elementos = new List<ProductoColasCirculares>();
+
+            if (!EstaVacia())
+            {
+                int i = frente;
+                int elementosContados = 0;
+
+                while (elementosContados < contador)
+                {
+                    elementos.Add(cola[i]);
+                    i = (i + 1) % tamaño;
+                    elementosContados++;
+                }
+            }
+
+            return elementos;
+        }
+    }
+}
---
+++ ProductoColasCirculares.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProyectoFinalEstructuraDatosGrupo4
+{
+    internal class ProductoColasCirculares
+    {
+        public string Codigo { get; set; }
+        public string NombreProducto { get; set; }
+        public string Descripcion { get; set; }
+        public string Categoria { get; set; }
+        public DateTime FechaIngreso { get; set; }
+        public DateTime FechaVencimiento { get; set; }
+        public decimal PrecioCompra { get; set; }
+        public decimal PrecioVenta { get; set; }
+
+        // Constructor para inicializar el producto
+        public ProductoColasCirculares(
+            string codigo, string producto, string descripcion,
+            string categoria, DateTime fechaIngreso, DateTime fechaVencimiento,
+            decimal precioCompra, decimal precioVenta
+            )
+        {
+            Codigo = codigo;
+            NombreProducto = producto;
+            Descripcion = descripcion;
+            Categoria = categoria;
+            FechaIngreso = fechaIngreso;
+            FechaVencimiento = fechaVencimiento;
+            PrecioCompra = precioCompra;
+            PrecioVenta = precioVenta;
+        }
+
+
+        // Sobreescribir ToString para mostrar fácilmente el producto en la lista
+        public override string ToString()
+        {
+            return $"Código: {Codigo}, Producto: {NombreProducto}, Categoría: {Categoria}, Precio Venta: ${PrecioVenta}";
+        }
+    }
+}
